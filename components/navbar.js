603b74d7490d01a17c038c456379d5ff84bf2139
earthrise8 <flyingpig071@gmail.com>
2025-07-30 11:12:15 -0700

    Update navbar.js

--- components/navbar.js
+++ components/navbar.js
@@ -1,24 +1,3 @@
-/* export function initNavbar() {
-  const modeToggle = document.getElementById('mode-toggle');
-  const mobileModeToggle = document.getElementById('mobile-mode-toggle');
-  const sidebarToggle = document.getElementById('menu-toggle');
-  const mobileMenu = document.getElementById('mobile-menu'); */
-/*
-  function toggleDarkMode() {
-    document.body.classList.toggle('light-mode');
-    const icon = document.body.classList.contains('light-mode') ? '☀️' : '🌙';
-    if (modeToggle) modeToggle.textContent = icon;
-  }
-
-  if (modeToggle) modeToggle.addEventListener('click', toggleDarkMode);
-/*
-  if (sidebarToggle && mobileMenu) {
-    sidebarToggle.addEventListener('click', () => {
-      mobileMenu.classList.toggle('hidden'); 
-    });
-  }
-}
-*/
 
 document.addEventListener("DOMContentLoaded", () => {
   const hamburger = document.getElementById("hamburger");
