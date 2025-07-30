8d20ecbb26eb8a1277000041053167de5fc51163
earthrise8 <flyingpig071@gmail.com>
2025-07-30 11:11:17 -0700

    Update init.js

--- components/init.js
+++ components/init.js
@@ -2,14 +2,6 @@ const base = '/home/components/';
 
 export async function loadLayout() {
   try {
-    /*
-    const navRes = await fetch(`${base}navbar.html`);
-    const navHTML = await navRes.text();
-    document.getElementById("navbar-container").innerHTML = navHTML;
-
-    const navbarModule = await import(`${base}navbar.js`);
-    navbarModule.initNavbar();
-    */
     const footerRes = await fetch(`${base}footer.html`);
     const footerHTML = await footerRes.text();
     document.getElementById("footer-container").innerHTML = footerHTML;
